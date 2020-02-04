d2886400011633322718846c7acaf1a866a2e185
Matt Geimer <matasgeimer@gmail.com>
2020-02-04 17:37:47 -0600

    Initial Commit

---
+++ Stocks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stocks
+//
+//  Created by Matthew Geimer on 2/4/20.
+//  Copyright © 2020 SIGMobile. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
