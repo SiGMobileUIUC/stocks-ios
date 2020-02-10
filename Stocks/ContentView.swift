27283916ece255947cf392abe76ec56256580b8c
Matt Geimer <matasgeimer@gmail.com>
2020-02-10 14:55:48 -0600

    Added functionality such that button now calls API Added Text() labels that present data retrieved from API

--- Stocks/ContentView.swift
+++ Stocks/ContentView.swift
@@ -9,9 +9,61 @@
 import SwiftUI
 
 struct ContentView: View {
+	
+	private let API_TOKEN: String = "pk_243b38b37f924b15b0eaaf0ab9a57d5f"
+	
+	@State private var currentCompanyTicker: String = "AAPL"
+	@State private var companyLoaded = false
+	
+	@State private var companyName:String = ""
+	@State private var iexRealtimePrice: Double = 0
+	@State private var volume: Int = 0
+	
     var body: some View {
-        Text("Hello, World!")
+		VStack {
+			TextField("Stock Ticker", text: $currentCompanyTicker)
+				.foregroundColor(Color.white)
+				.padding()
+				.background(Color.blue)
+				.clipShape(RoundedRectangle(cornerRadius: 10))
+				.padding(12)
+				.shadow(radius: 10)
+			if companyLoaded {
+				Group {
+					Text("Company Name: \(companyName)")
+					Text("Latest Price: $\(iexRealtimePrice)")
+					Text("Volume: \(volume)")
+				}
+					.padding(.top, 30)
+			}
+			Button(action: {
+				if let url = URL(string: "https://cloud.iexapis.com/stable/stock/\(self.currentCompanyTicker)/quote?token=\(self.API_TOKEN)") {
+				   URLSession.shared.dataTask(with: url) { data, response, error in
+					  if let data = data {
+						  do {
+							 let res = try JSONDecoder().decode(Response.self, from: data)
+							self.companyName = res.companyName
+							self.iexRealtimePrice = res.iexRealtimePrice
+							self.volume = res.iexVolume
+							self.companyLoaded = true
+						  } catch let error {
+							 print(error)
+						  }
+					   }
+				   }.resume()
+				}
+			}) {
+				Text("Make request")
+					.padding()
+			}
+		}
     }
+}
+
+struct Response: Codable {
+	let companyName: String
+	let iexRealtimePrice: Double
+	let iexVolume: Int
 }
 
 struct ContentView_Previews: PreviewProvider {
