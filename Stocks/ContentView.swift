641399510a242c9507b83464d2c73614c2c27456
Matt Geimer <matasgeimer@gmail.com>
2020-02-11 17:49:20 -0600

    [refactor] Moved data to separate view model.

--- Stocks/ContentView.swift
+++ Stocks/ContentView.swift
@@ -6,55 +6,84 @@
 //  Copyright © 2020 SIGMobile. All rights reserved.
 //
 
+//Intraday prices link: https://cloud.iexapis.com/stable/stock/AAPL/intraday-prices?token=pk_243b38b37f924b15b0eaaf0ab9a57d5f
+
 import SwiftUI
 
-struct ContentView: View {
+class ViewModel: ObservableObject {
 	
 	private let API_TOKEN: String = "pk_243b38b37f924b15b0eaaf0ab9a57d5f"
 	
-	@State private var currentCompanyTicker: String = "AAPL"
-	@State private var companyLoaded = false
+	var currentCompanyTicker: String
+	@Published var companyLoaded: Bool
+	@Published var companyName: String
+	@Published var iexRealtimePrice: Double
+	@Published var volume: Int
 	
-	@State private var companyName:String = ""
-	@State private var iexRealtimePrice: Double = 0
-	@State private var volume: Int = 0
+	init() {
+		companyLoaded = false
+		companyName = ""
+		iexRealtimePrice = 0
+		volume = 0
+		currentCompanyTicker = ""
+	}
+	
+	func getDataFromAPI() {
+		if let url = URL(string: "https://cloud.iexapis.com/stable/stock/\(self.currentCompanyTicker)/quote?token=\(self.API_TOKEN)") {
+		   URLSession.shared.dataTask(with: url) { data, response, error in
+			  if let data = data {
+				  do {
+					 let res = try JSONDecoder().decode(Response.self, from: data)
+					DispatchQueue.main.async {
+						self.companyName = res.companyName
+						self.iexRealtimePrice = res.iexRealtimePrice
+						self.volume = res.iexVolume
+						self.companyLoaded = true
+					}
+				  } catch let error {
+					 print(error)
+				  }
+			   }
+		   }.resume()
+		}
+	}
+}
+
+struct ContentView: View {
+	
+	@ObservedObject var viewModel: ViewModel
+	
+	init(viewModel: ViewModel) {
+		self.viewModel = viewModel
+		viewModel.currentCompanyTicker = ""
+	}
 	
     var body: some View {
 		VStack {
-			TextField("Stock Ticker", text: $currentCompanyTicker)
-				.foregroundColor(Color.white)
+			TextField("Stock Ticker", text: $viewModel.currentCompanyTicker)
 				.padding()
-				.background(Color.blue)
+				.background(Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0))
 				.clipShape(RoundedRectangle(cornerRadius: 10))
 				.padding(12)
 				.shadow(radius: 10)
-			if companyLoaded {
+			if viewModel.companyLoaded {
 				Group {
-					Text("Company Name: \(companyName)")
-					Text("Latest Price: $\(iexRealtimePrice)")
-					Text("Volume: \(volume)")
+					Text("Company Name: \(viewModel.companyName)")
+					Text("Latest Price: $\(viewModel.iexRealtimePrice)")
+					Text("Volume: \(viewModel.volume)")
 				}
 					.padding(.top, 30)
 			}
 			Button(action: {
-				if let url = URL(string: "https://cloud.iexapis.com/stable/stock/\(self.currentCompanyTicker)/quote?token=\(self.API_TOKEN)") {
-				   URLSession.shared.dataTask(with: url) { data, response, error in
-					  if let data = data {
-						  do {
-							 let res = try JSONDecoder().decode(Response.self, from: data)
-							self.companyName = res.companyName
-							self.iexRealtimePrice = res.iexRealtimePrice
-							self.volume = res.iexVolume
-							self.companyLoaded = true
-						  } catch let error {
-							 print(error)
-						  }
-					   }
-				   }.resume()
-				}
+				self.viewModel.getDataFromAPI()
 			}) {
 				Text("Make request")
 					.padding()
+					.foregroundColor(Color.white)
+					.background(Color.blue)
+					.clipShape(RoundedRectangle(cornerRadius: 10))
+					.padding(12)
+					.shadow(radius: 10)
 			}
 		}
     }
@@ -68,6 +97,6 @@ struct Response: Codable {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(viewModel: ViewModel())
     }
 }
